90ce497f274051da0b1cc33feace0dbb4a70b6cd
Sude <sudesavas_@hotmail.com>
2025-12-24 22:01:58 +0300

    Create WatchlistDao skeleton and setup database connection.

---
+++ src/DataBase/Dao/WatchlistDao.java
@@ -0,0 +1,12 @@
+package DataBase.Dao;
+
+import DataBase.DataBaseManager;
+
+public class WatchlistDao {
+
+    private DataBaseManager db_manager;
+
+    public WatchlistDao() {
+        db_manager = new DataBaseManager();
+    }
+}
