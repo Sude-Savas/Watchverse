667d5d197a4eeb860ebd43e02c0d73689a20b223
Sude <sudesavas_@hotmail.com>
2025-12-24 22:40:49 +0300

    Create WatchlistDao with SQL queries for list management

--- src/DataBase/Dao/WatchlistDao.java
+++ src/DataBase/Dao/WatchlistDao.java
@@ -1,6 +1,11 @@
 package DataBase.Dao;
 
 import DataBase.DataBaseManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WatchlistDao {
 
@@ -8,5 +13,67 @@ public class WatchlistDao {
 
     public WatchlistDao() {
         db_manager = new DataBaseManager();
+    }
+
+    public boolean createWatchlist(String username, String listName) throws SQLException {
+        /* Default visibility is private
+           Using subquery to avoid a separate SELECT query.
+        */
+        String sql = "INSERT INTO watchlists (user_id, name, visibility) " +
+                "VALUES ((SELECT id FROM users WHERE username = ?), ?, 'PRIVATE')";
+
+        try (PreparedStatement preparedStatement =
+                     db_manager.getConnection().prepareStatement(sql)) {
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, listName);
+
+            return preparedStatement.executeUpdate() == 1;
+        }
+    }
+
+    //Gets all the watchlist titles that user has
+    public List<String> getUserWatchlists(String username) throws SQLException {
+        List<String> watchlists = new ArrayList<>();
+
+        // Used JOIN to combine User and Watchlist tables
+        String sql = "SELECT w.name FROM watchlists w " +
+                "JOIN users u ON w.user_id = u.id " +
+                "WHERE u.username = ?";
+
+        try (PreparedStatement preparedStatement =
+                     db_manager.getConnection().prepareStatement(sql)) {
+
+            preparedStatement.setString(1, username);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            //Adding every watchlist in results to the list
+            while (resultSet.next()) {
+                watchlists.add(resultSet.getString("name"));
+            }
+        }
+        return watchlists;
+    }
+
+    //Helper method
+    public int getWatchlistId(String username, String listName) throws SQLException {
+
+        //Used JOIN because listName might not be unique
+        String sql = "SELECT w.id FROM watchlists w " +
+                "JOIN users u ON w.user_id = u.id " +
+                "WHERE u.username = ? AND w.name = ?";
+
+        try (PreparedStatement ps = db_manager.getConnection().prepareStatement(sql)) {
+            ps.setString(1, username);
+            ps.setString(2, listName);
+            ResultSet rs = ps.executeQuery();
+
+            //If there is a result from the database, get the ID
+            if (rs.next()) {
+                return rs.getInt("id");
+            }
+        }
+        //If it fails we return -1
+        return -1;
     }
 }
