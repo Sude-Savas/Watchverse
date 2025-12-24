45a33e6bdb283c2c32291cebf280e89aa942b921
Sude <sudesavas_@hotmail.com>
2025-12-24 21:54:49 +0300

    Added Server class to accept connections and start ClientHandler threads

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,4 +1,28 @@
 package Server;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+
+    private static final int port = 4870;
+
+    public static void main(String[] args){
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            //Server will wait for client all the time
+            while(true){
+                //Accept when there is a request from a client
+                Socket clientSocket = serverSocket.accept();
+
+                //Thread to take care of the client
+                ClientHandler handler = new ClientHandler(clientSocket);
+                new Thread(handler).start();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
